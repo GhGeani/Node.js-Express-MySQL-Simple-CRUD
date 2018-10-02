a391328f27c233309e88894a82f9a5a38c46d9df
Gherghe Geani <gherghe_geani@yahoo.com>
2018-10-03 02:38:02 +0300

    Edit/delete confirmation

---
+++ public/assets/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.edit-form').submit(function(evt){
+        //evt.preventDefault();
+        if(confirm('This profile will be edited...')){
+            //$(".edit-form").unbind('submit');
+        }else{
+            return false;
+        }
+    });
+
+    $('#delBtn').on('click', function(evt){
+        //evt.preventDefault();
+        if(confirm('This profile will be deleted...')){
+            //$(this).unbind('click');
+        }else{
+            return false;
+        }
+    });
+});
